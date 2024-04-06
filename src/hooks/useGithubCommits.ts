249d71db8960a970ecff7a202265e896a2c3e11e
bh-sdev <hicksbrian.sdev@gmail.com>
2024-04-05 21:11:52 -0700

    Update loading and error handling, Update read me

--- src/hooks/useGithubCommits.ts
+++ src/hooks/useGithubCommits.ts
@@ -18,6 +18,7 @@ export const useGithubCommits = ({
 
   useEffect(() => {
     async function FetchCommits() {
+      setLoading(true);
       const res = await fetch(
         `https://api.github.com/repos/${repoOwnerUsername}/${repoName}/commits`,
         {
@@ -27,14 +28,15 @@ export const useGithubCommits = ({
             "X-GitHub-Api-Version": "2022-11-28",
           },
         }
-      );
+      ).catch((err) => setError(err));
 
-      const data = await res.json();
+      const data = await res?.json();
 
       setData(data);
+      setLoading(false);
     }
 
-    if (!data) {
+    if (!loading) {
       FetchCommits();
     }
   }, []);
